0af309225b4731d990db45353782d5ab8795c6fb
Shishir435 <shishirchaurasiya435@gmail.com>
2024-02-17 06:00:25 +0530

    herosection clean

--- src/app/(home)/_components/hero-section.tsx
+++ src/app/(home)/_components/hero-section.tsx
@@ -7,25 +7,8 @@ const HeroSection = () => {
   return (
     <section id="#about" className="overflow-x-hidden">
       <StarsCanvas />
-      {/* <div className="grid grid-cols-1 lg:grid-cols-5 place-content-between">
-        <div className="col-span-5 lg:col-span-2 flex flex-col items-center justify-center h-screen">
-          <Image
-            src="/inbound.png"
-            height={261}
-            width={637}
-            alt="inbound image"
-            className="mix-blend-screen"
-          />
-          <h1 className="text-3xl">{siteConfig.siteSlogan}</h1>
-        </div>
-        <div className="  h-full w-full col-span-5 lg:col-span-3 lg:flex lg:justify-center">
-          <div className="w-full h-full flex justify-center">
-            <EarthCanvas />
-          </div>
-        </div>
-      </div> */}
       <div className="flex flex-wrap justify-center items-center">
-        <div className="w-full lg:w-1/2 flex flex-col items-center justify-center h-[30vh] lg:h-screen px-6 md:px-5 lg:px-0">
+        <div className="w-full lg:w-1/2 flex flex-col items-center justify-center h-[35vh] lg:h-screen px-8 md:px-5 lg:px-0">
           <Image
             src="/inbound.png"
             height={261}
