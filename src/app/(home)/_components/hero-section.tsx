3c904d90eec1d6d19d9a102ca0c9142940d0f2b1
Shishir435 <shishirchaurasiya435@gmail.com>
2024-04-10 14:44:09 +0530

    remove earth and update team details

--- src/app/(home)/_components/hero-section.tsx
+++ src/app/(home)/_components/hero-section.tsx
@@ -12,7 +12,7 @@ const HeroSection = () => {
     <section id="#about" className="overflow-x-hidden">
       <StarsCanvas />
       <div className="flex flex-wrap justify-center items-center">
-        <div className="w-full lg:w-1/2 flex flex-col items-center justify-center h-[35vh] lg:h-screen px-8 md:px-5 lg:px-0">
+        <div className="w-full flex flex-col items-center justify-center h-[35vh] lg:h-screen px-8 md:px-5 lg:px-0">
           <Image
             src="/inbound.png"
             height={261}
@@ -23,11 +23,11 @@ const HeroSection = () => {
           />
           <h1 className={`text-3xl md:text-5xl ${myFont.className}`}>{siteConfig.siteSlogan}</h1>
         </div>
-        <div className="  h-full w-full lg:w-1/2 flex justify-center items-center">
+        {/* <div className="  h-full w-full lg:w-1/2 flex justify-center items-center">
           <div className="w-full h-[400px] sm:h-[600px]  md:h-[800px] my-auto flex justify-center items-center">
             <EarthCanvas />
           </div>
-        </div>
+        </div> */}
       </div>
     </section>
   );
